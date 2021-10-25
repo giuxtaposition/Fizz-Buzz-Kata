3b8dba20c65f6e450f0afe25bd9fbed81577d6e4
giuxtaposition <yg97.cs@gmail.com>
2021-10-25 17:03:10 +0200

    added tests

---
+++ tests/Fizz-Buzz.test.ts
@@ -0,0 +1,13 @@
+describe('FizzBuzz:', () => {
+    test('For multiple of Three print Fizz', () => {
+        expect(FizzBuzz.parse(3)).toEqual('Fizz')
+    })
+
+    test('For multiple of Five print Buzz', () => {
+        expect(FizzBuzz.parse(5)).toEqual('Buzz')
+    })
+
+    test('For multiple of both Three and Five print FizzBuzz', () => {
+        expect(FizzBuzz.parse(15)).toEqual('FizzBuzz')
+    })
+})
